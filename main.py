b27baf3e3395030bdd4413c5107694515a70903f
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-29 23:29:35 -0300

    funcoes de ligar, desligar e alternar

---
+++ main.py
@@ -0,0 +1,14 @@
+from discover import discover_yeelight_bulbs
+from methods import BulbController
+
+
+bulbs = discover_yeelight_bulbs()
+
+if not bulbs:
+    print("Nenhuma lâmpada encontrada.")
+else:
+    print("Encontradas:", bulbs)
+    ip, port = bulbs[0]
+
+    controller = BulbController(ip, port)
+    controller.toggle()
---
+++ methods.py
@@ -0,0 +1,53 @@
+import socket
+import json
+
+class BulbController:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
+
+    def turn_on(self):
+        cmd = {
+            "id": 1,
+            "method": "set_power",
+            "params": ["on", "smooth", 500]
+        }
+
+        msg = (json.dumps(cmd) + "\r\n").encode()
+         
+        self.socket.connect((self.ip, self.port))
+        self.socket.send(msg)
+
+        self.socket.recv(1024)
+        self.socket.close()
+
+    def turn_off(self):
+        cmd = {
+            "id": 1,
+            "method": "set_power",
+            "params": ["off", "smooth", 500]
+        }
+
+        msg = (json.dumps(cmd) + "\r\n").encode()
+
+        self.socket.connect((self.ip, self.port))
+        self.socket.send(msg)
+        
+        self.socket.recv(1024)
+        self.socket.close()
+
+    def toggle(self):
+        cmd = {
+            "id": 1,
+            "method": "toggle",
+            "params": []
+        }
+
+        msg = (json.dumps(cmd) + "\r\n").encode()
+
+        self.socket.connect((self.ip, self.port))
+        self.socket.send(msg)
+
+        self.socket.recv(1024)
+        self.socket.close()
